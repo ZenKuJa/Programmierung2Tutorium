31393bdc0b6f32de214a7bfaaa7d00efb6f3dfe1
ianthecake <fabian.bauriedl@icloud.com>
2025-05-26 11:20:04 +0200

    Added lambda

--- ExceptionsSolutions/src/InvalidAgeException.java
+++
@@ -1,6 +0,0 @@
-// Exercise 1 Custom Exception
-class InvalidAgeException extends Exception {
-    public InvalidAgeException(String message) {
-        super(message);
-    }
-}
---
+++ LambdaExercises/src/Interfaces.java
@@ -0,0 +1,15 @@
+package src;
+
+// AUFGABE 1: Hello
+
+// AUFGABE 2: PrintName
+
+// AUFGABE 3: WordLength
+
+// AUFGABE 4: PrintPersonAge
+
+// AUFGABE 5: Add
+
+// AUFGABE 6: RandomNumber
+
+// AUFGABE 7: IsShortWord
